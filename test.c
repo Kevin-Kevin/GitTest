e5399c9367aeae45e9fed95aef436b18db4bd715
kevin-kevin <10041116510@qq.com>
2022-01-25 13:54:18 +0800

    other update

--- test.c
+++ test.c
@@ -1,14 +1,4 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int main(){
-  char str[5];
-  char str2[5];
-  scanf("%s", str);
-  scanf("%s",str2);
-
-  printf("str = %s , srt2 = %s",str,str2);
-  printf("srt len = %d",strlen(str));
-  
-  return 0;
-}
+另一个更改
